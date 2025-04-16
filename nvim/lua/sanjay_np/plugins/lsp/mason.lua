27265f764a0f3335f799c3f367d032f4de6f254c
sanjay-np <excelblade10@gmail.com>
2025-04-16 23:13:43 +0545

    chore:removed unnecessary modules

--- nvim/lua/sanjay_np/plugins/lsp/mason.lua
+++ nvim/lua/sanjay_np/plugins/lsp/mason.lua
@@ -2,12 +2,18 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		-- import mason
 		local mason = require("mason")
+
+		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
 
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
 		mason.setup({
 			ui = {
 				icons = {
@@ -21,16 +27,25 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
+				"ts_ls",
 				"html",
 				"cssls",
 				"tailwindcss",
 				"lua_ls",
-				"emmet_language_server",
-				"phpactor",
+				"emmet_ls",
 				"intelephense",
-				"ts_ls",
 			},
 			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"eslint_d",
+                "php-cs-fixer",
+                "phpstan",
+			},
 		})
 	end,
 }
