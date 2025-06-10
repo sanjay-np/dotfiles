40892d9d6275ff51ead3553331b8b0211af0f824
sanjay-np <excelblade10@gmail.com>
2025-06-10 13:55:05 +0545

    chore: minor config changed

--- nvim/lua/sanjay_np/plugins/colorscheme.lua
+++ nvim/lua/sanjay_np/plugins/colorscheme.lua
@@ -5,9 +5,29 @@ return {
         priority = 1000,
         config = function()
             vim.g.sonokai_transparent_background = 1
-            vim.g.sonokai_disable_italic_comment = 1
+            vim.g.sonokai_disable_italic_comment = 0
             vim.g.sonokai_enable_italic = false
-            vim.cmd.colorscheme('sonokai')
+            vim.g.sonokai_style = "atlantis"
+            -- vim.cmd.colorscheme('sonokai')
         end
     },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                style = "moon",
+                transparent = true,
+                styles = {
+                    comments = { italic = true },
+                    keywords = { italic = false },
+                    functions = {},
+                    variables = {},
+                },
+            })
+            vim.cmd.colorscheme('tokyonight')
+        end
+    }
 }
--- nvim/lua/sanjay_np/plugins/indentation.lua
+++ nvim/lua/sanjay_np/plugins/indentation.lua
@@ -1,10 +1,14 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	main = "ibl",
-	opts = {
-		indent = {
-			char = "│",
-		},
-	},
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+        indent = {
+            char = "│",
+        },
+        scope = {
+            show_start = false,
+            show_end = false,
+        },
+    },
 }
