d4d51da8e4e3242d23339a2896c5d1d01932b632
sanjay-np <excelblade10@gmail.com>
2025-08-16 19:15:16 +0545

    chore: new changes

--- nvim/lua/plugins/ai-assistant.lua
+++ nvim/lua/plugins/ai-assistant.lua
@@ -11,4 +11,40 @@ return {
 			})
 		end,
 	},
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "openrouter",
+					},
+					inline = {
+						adapter = "openrouter",
+					},
+				},
+				adapters = {
+					openrouter = function()
+						return require("codecompanion.adapters").extend("openai_compatible", {
+							env = {
+								url = "https://openrouter.ai/api",
+								api_key = "",
+								chat_url = "/v1/chat/completions",
+							},
+							schema = {
+								model = {
+									default = "qwen/qwen3-coder:free",
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 }
