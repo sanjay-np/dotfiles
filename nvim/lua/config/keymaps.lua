28a164f6aaeb61f26500983291441ad829910326
Sanjay Chaudhary <excelblade10@gmail.com>
2026-02-21 09:30:14 +0545

    Add keymaps for buffer resizing in Neovim
    Added key mappings for resizing buffer width and height.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,11 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Buffer Width
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase buffer width" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -5<CR>", { desc = "Decrease buffer width" })
+
+-- Buffer Height
+keymap.set("n", "<C-Down>", "<cmd>resize +5<CR>", { desc = "Increase buffer height" })
+keymap.set("n", "<C-Up>", "<cmd>resize -5<CR>", { desc = "Decrease buffer height" })
