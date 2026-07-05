96938fc55b5c93157056444c9626a18fff2286d1
sanjay-np <excelblade10@gmail.com>
2026-07-05 08:16:02 +0545

    chore: hyprland config update with noctalia shell setup

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -4,9 +4,16 @@
 ------------------
 -- Montior wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "",
-	mode = "preferred",
-	position = "auto",
+	output = "HDMI-A-1",
+	mode = "1920x1080@120",
+	position = "0x0",
+	scale = "1",
+})
+
+hl.monitor({
+	output = "eDP-1",
+	mode = "1920x1200@60",
+	position = "0x1080",
 	scale = "1",
 })
 
@@ -28,7 +35,7 @@ BROWSER = "firefox"
 -- if you dont use UWSM add your auto start programs here, otherwise use XDG autostart https://wiki.archlinux.org/title/XDG_Autostart
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("qs -c noctalia-shell")
+	hl.exec_cmd("noctalia")
 	hl.exec_cmd("dbus-update-activation-environment --systemd --all")
 	hl.exec_cmd("xhost +SI:localuser:root")
 	hl.exec_cmd("hyprctl setcursor Adwaita 24")
@@ -105,19 +112,12 @@ hl.config({
 		blur = {
 			enabled = true,
 			size = 5,
-			passes = 3,
-			ignore_opacity = true,
-
-			noise = 0.08,
-			contrast = 1.5,
-			brightness = 0.8,
+			passes = 1,
 			special = true,
 
 			xray = true,
 			new_optimizations = true,
 			popups = true,
-
-			vibrancy = 0.1696,
 		},
 	},
 	animations = {
@@ -127,16 +127,10 @@ hl.config({
 
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 -- Default beziers
-hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
-hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
-hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
 hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
-hl.curve("overshoot", { type = "bezier", points = { { 0.5, 0.9 }, { 0.1, 1.1 } } })
 
 -- Default springs
 hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-hl.curve("rubber", { type = "spring", mass = 1, stiffness = 70, dampening = 10 })
 
 -- Animations
 hl.animation({ leaf = "global", enabled = true, speed = 3, bezier = "quick" })
@@ -189,8 +183,7 @@ hl.config({
 		repeat_rate = 50,
 
 		follow_mouse = 1,
-		sensitivity = 0.8, -- -1.0 - 1.0, 0 means no modification.
-		accel_profile = "flat",
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
 		touchpad = {
 			natural_scroll = false,
@@ -208,12 +201,12 @@ hl.gesture({ fingers = 3, direction = "left", action = "float" })
 ---------------------
 
 local mainMod = "SUPER"
-local noctCall = "qs -c noctalia-shell ipc call "
+local ipc = "noctalia msg"
 -- local launchPrefix = "uwsm app -- " -- if you are not using UWSM, make this empty (e.g. "")
 local launchPrefix = "" -- if you are not using UWSM, make this empty (e.g. "")
 
 hl.bind(
-	mainMod .. " + M",
+	mainMod .. "+ ALT + DELETE",
 	hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
 )
 
@@ -228,8 +221,8 @@ hl.bind(mainMod .. " + SHIFT + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + M", hl.dsp.window.fullscreen({ mode = 1 }))
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))
 
-hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(noctCall .. " lockScreen lock"))
-hl.bind(mainMod .. " + ALT + C", hl.dsp.exec_cmd(noctCall .. " sessionMenu toggle"))
+-- hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(noctCall .. " lockScreen lock"))
+-- hl.bind(mainMod .. " + ALT + C", hl.dsp.exec_cmd(noctCall .. " sessionMenu toggle"))
 
 -- Change focus
 hl.bind(mainMod .. " + Left", hl.dsp.focus({ direction = "left" }))
@@ -257,49 +250,33 @@ hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize())
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(launchPrefix .. TERMINAL))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(launchPrefix .. FILE_MANAGER))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(launchPrefix .. BROWSER))
-hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(launchPrefix .. TERMINAL .. "-e antigravity-ide"))
--- hl.bind(mainMod .. " + C", hl.dsp.exec_cmd(launchPrefix .. CALCULATOR))
--- hl.bind("CONTROL + SHIFT + Escape", hl.dsp.exec_cmd(launchPrefix .. TERMINAL .. " -e btop"))
--- hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd(noctCall .. "settings toggle"))
--- hl.bind(mainMod .. " + X", hl.dsp.exec_cmd(noctCall .. "controlCenter toggle"))
-hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd(noctCall .. "launcher toggle"))
-hl.bind(mainMod .. " + period", hl.dsp.exec_cmd(noctCall .. "launcher emoji"))
+
+hl.bind(mainMod .. "+Space", hl.dsp.exec_cmd(ipc .. " panel-toggle launcher"))
+hl.bind(mainMod .. "+P", hl.dsp.exec_cmd(ipc .. " panel-toggle control-center"))
+hl.bind(mainMod .. "+comma", hl.dsp.exec_cmd(ipc .. " settings-toggle"))
+hl.bind(mainMod .. "+W", hl.dsp.exec_cmd(ipc .. " panel-toggle wallpaper"))
 
 ---------------------------
 ---- HARDWARE CONTROLS ----
 ---------------------------
-
 -- Audio
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(noctCall .. "volume increase"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(noctCall .. "volume decrease"), { locked = true, repeating = true })
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd(noctCall .. "volume muteOutput"), { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd(noctCall .. "volume muteInput"), { locked = true, repeating = true })
-
--- Media
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(noctCall .. "media playPause"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd(noctCall .. "media playPause"), { locked = true })
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd(noctCall .. "media next"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(noctCall .. "media previous"), { locked = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(ipc .. " volume-up"))
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(ipc .. " volume-down"))
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd(ipc .. " volume-mute"))
 
 -- Brightness
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(noctCall .. "brightness increase"), { repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(noctCall .. "brightness decrease"), { repeating = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(ipc .. " brightness-up"))
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(ipc .. " brightness-down"))
+
+-- Screen Capture
+hl.bind("Print", hl.dsp.exec_cmd(ipc .. " screenshot-region"))
 
 -------------------
 ---- UTILITIES ----
 -------------------
 
--- Screen Capture
-hl.bind(mainMod .. " + P", hl.dsp.exec_cmd(noctCall .. "plugin:screen-toolkit colorPicker"))
-hl.bind("Print", hl.dsp.exec_cmd(noctCall .. "plugin:screen-toolkit annotate"))
-hl.bind(mainMod .. " + Print", hl.dsp.exec_cmd(noctCall .. "plugin:screen-toolkit annotateWindow"))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(noctCall .. "plugin:screen-toolkit toggle"))
-
--- Theming and Wallpaper
-hl.bind(mainMod .. " + SHIFT + W", hl.dsp.exec_cmd(noctCall .. " wallpaper toggle"))
-
 -- Clipboard
-hl.bind(mainMod .. " + V", hl.dsp.exec_cmd(noctCall .. "launcher clipboard"))
+-- hl.bind(mainMod .. " + V", hl.dsp.exec_cmd(noctCall .. "launcher clipboard"))
 
 --------------------
 ---- WORKSPACES ----
@@ -310,6 +287,14 @@ for i = 1, 10 do
 	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
 	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i, follow = false }))
 	-- hl.bind(mainMod .. " + ALT + " .. key, hl.dsp.window.move({ workspace = i, follow = true }))
+end
+
+-- Persistent workspaces per monitor
+for i = 1, 5 do
+	hl.workspace_rule({ workspace = i, monitor = "eDP-1", persistent = true })
+end
+for i = 6, 10 do
+	hl.workspace_rule({ workspace = i, monitor = "HDMI-A-1", persistent = true })
 end
 
 hl.bind(mainMod .. "+ Tab", hl.dsp.focus({ workspace = "previous" }))
@@ -332,7 +317,7 @@ hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special())
 ---- NOTIFICATIONS ----
 -----------------------
 
-hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(noctCall .. "notifications toggleHistory"))
+-- hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(noctCall .. "notifications toggleHistory"))
 
 -- Picture-in-Picture
 hl.window_rule({
@@ -450,6 +435,14 @@ end
 
 hl.window_rule({ match = { float = true }, move = "50% 50%" })
 
+-- Android Emulator
+hl.window_rule({
+	match = { class = "^(Emulator)$" },
+	float = true,
+	center = true,
+	opacity = "1.0 override",
+})
+
 -- Float Common Modals
 local modalMatches = {
 	{
@@ -489,3 +482,6 @@ hl.window_rule({
 
 	no_focus = true,
 })
+
+-- For Noctalia Color templates
+require("noctalia").apply_theme()
